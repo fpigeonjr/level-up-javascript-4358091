9a36136b7e453f2d02da311ae58552e23895b11c
Frank Pigeon Jr. <frank.pigeonjr@gmail.com>
2025-04-26 16:34:12 +0000

    feat: adds Movie class

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,29 @@
-// Write your code here
+class Movie {
+  /**
+   * 
+   * @param {string} title 
+   * @param {string} director 
+   * @param {'Action' | 'Drama' | 'Horror' | 'SciFi'} genre 
+   * @param {number} releaseYear 
+   * @param {1 | 2 | 3 | 4 | 5} rating 
+   */
+  constructor(title, director, genre, releaseYear, rating){
+    this.title = title
+    this.director = director
+    this.genre = genre
+    this.releaseYear = releaseYear
+    this.rating = rating
+  }
+
+  get overview() {
+    return this.getOverview
+  }
+  
+  getOverview() {
+    return (`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. 
+It received a rating of ${this.rating}`)    
+  }
+}
+
+const braveHeart = new Movie("Brave Heart", "Mel Gibson", "Action", 2012, 5)
+console.log(braveHeart.overview())
