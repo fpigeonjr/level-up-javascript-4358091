9a919e4ca2e033b9a1faeaa750aeb7de2e7affc2
Frank Pigeon Jr <fpigeon@flexion.us>
2025-04-29 09:04:51 -0500

    feat: completes atttendance challenge

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,19 @@
-// Write your code here
+// class User {
+//   #userName;
+//   #password;
+//   constructor(userName, password, age) {
+//     this.#userName = userName;
+//     this.#password = password;
+//     this.age = age;
+//   }
+// }
+
+// const Frank = new User("Frank", "password123", 45);
+// console.log(Frank.age);
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = { [username]: "Frank", [password]: "12345", age: 45 };
+console.log(user.username); // undefined
+console.log(user.password); // undefined
--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,44 @@
-// Write your code here
+class Class {
+  constructor(classroom) {
+    this.classroom = classroom;
+  }
+
+  getTeacher() {
+    const { _, classList } = this.classroom;
+    const [teacher] = classList;
+    return `The teacher is ${teacher}`;
+  }
+
+  getTA() {
+    const { hasTeachingAssistant, classList } = this.classroom;
+    if (!hasTeachingAssistant) {
+      return "No Teaching Assistant";
+    } else {
+      const [_, TA] = classList;
+      return `The teaching assistant is ${TA}`;
+    }
+  }
+
+  getStudents() {
+    const { hasTeachingAssistant, classList } = this.classroom;
+
+    if (hasTeachingAssistant) {
+      const [_, __, ...students] = classList;
+      return students;
+    } else {
+      const [_, ...students] = classList;
+      return students;
+    }
+  }
+}
+
+const computerClassList = {
+  hasTeachingAssistant: false,
+  classList: ["Teacher Bob", "Joe", "Student 1", "Student 2", "Student 3"],
+};
+
+const myComputerClass = new Class(computerClassList);
+console.log(myComputerClass);
+console.log(myComputerClass.getTeacher());
+console.log(myComputerClass.getTA());
+console.log(myComputerClass.getStudents());
